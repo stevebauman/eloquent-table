b7d78158fb1d63baf4df0dcaab832dfe19c99e9e
Steve Bauman <steven_bauman_7@hotmail.com>
2014-11-27 14:40:38 -0500

    Added config file, added new function hidden, and updated documentation

---
+++ src/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 
+ * Eloquent Table Configuration File
+ * 
+ */
+
+return array(
+    
+    'default_table_attributes' => array(
+        'class' => 'table table-striped'
+    ),
+    
+    'default_hidden_column_attributes' => array(
+        'class' => 'hidden-xs',
+    ),
+    
+);
